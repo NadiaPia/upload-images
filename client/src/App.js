e6316b08d3ddd66a9aac61bc11bcdcc4bad07395
NadiaPia <206i417@gmail.com>
2023-05-03 23:57:18 -0700

    Add input for the upload functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const uploadImage = (files) => {    
+    console.log("files[0]", files[0]) //FileList {0: File, length: 1}
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        lang="en"
+        type="file"
+        onChange={(event) => {
+          uploadImage(event.target.files)
+        }}
+      />
     </div>
   );
 }
