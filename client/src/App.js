0caea1de77e81db5daf98d9aabe411ce4452b850
NadiaPia <206i417@gmail.com>
2023-05-04 00:33:22 -0700

    Add axios request to upload image to the cloudinary

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
 
+  const [imageSelected, setImageSelected] = useState()
+
   const uploadImage = (files) => {    
-    console.log("files[0]", files[0]) //FileList {0: File, length: 1}
-  }
+    //console.log("files[0]", files[0]) //FileList {0: File, length: 1}
+    const formData = new FormData();
+    formData.append("file", imageSelected);
+    formData.append("upload_preset", "kysjntpy");
+
+    axios.post("https://api.cloudinary.com/v1_1/dhq7myqzj/image/upload", formData).then((response) => {
+      console.log("response", response)
+    })
+  };
+
   return (
     <div>
       <input
         lang="en"
         type="file"
         onChange={(event) => {
-          uploadImage(event.target.files)
+          setImageSelected(event.target.files[0])
         }}
       />
+      <button onClick={uploadImage}> Upload Image </button>
     </div>
   );
 }
